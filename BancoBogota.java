420e3b7670f0521e39487b4f6385dcd8428dd443
jinnerzamora91 <jinnerzamora91@gmail.com>
2025-10-08 21:44:17 -0500

    Create BancoBogota.java
    recolecta informacion para crear cuenta externa Bogota

---
+++ BancoBogota.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nequiffa.app;
+
+public class BancoBogota extends CuentaBancaria {
+    public BancoBogota(String idCuenta, String nombreUsuario, String contraseña, double saldoInicial) {
+        super(idCuenta, nombreUsuario, contraseña, saldoInicial);
+    }
+
+    @Override
+    public double retirar(double monto) {
+        if (saldo >= monto) {
+            saldo -= monto;
+            System.out.println("✅ Retiro exitoso en Banco de Bogotá.");
+            return 0;
+        } else {
+            System.out.println("❌ Fondos insuficientes en Banco de Bogotá.");
+            return 0;
+        }
+    }
+}
